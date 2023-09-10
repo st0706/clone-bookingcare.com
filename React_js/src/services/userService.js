38dd50f7be10af93078d7f0c9a1ec6953ddf486e
Nguyen Son Tung <1stgemini123@gmail.com>
2023-09-10 18:45:36 +0700

    fetchApibyRedux

--- React_js/src/services/userService.js
+++ React_js/src/services/userService.js
@@ -24,4 +24,8 @@ const editUserService = (inputData) => {
     return axios.put('/api/edit-user', inputData)
 }
 
-export { handleLoginApi, getAllUsers, createNewUserService, deleteUserService, editUserService };
+const getAllCodeService = (inputData) => {
+    return axios.get(`/api/allcode?type=${inputData}`)
+}
+
+export { handleLoginApi, getAllUsers, createNewUserService, deleteUserService, editUserService, getAllCodeService };
